38d315faf2ac28ea14c4f6c1682ea5bb165581e0
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2021-05-16 13:43:03 +0200

    final

--- disks/views.py
+++ disks/views.py
@@ -7,10 +7,10 @@ def voir_albums(request):
 
 
 def voir_tracks(request, album_id):
-    return render(request, 'disks/voir_tracks.html', {'tracks': Track.objects.filter(album=album_id)})
+    return render(request, 'disks/voir_tracks.html',
+                  {'title': Album.objects.get(id=album_id).title, 'tracks': Track.objects.filter(album=album_id)})
 
 
 def voir_recherche(request, search):
     return render(request, 'disks/voir_recherche.html',
                   {'search': search, 'search_result': Album.objects.filter(title__contains=search)})
-
